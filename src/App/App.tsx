4c5f6597f1a6de92263a53b5fce6a38cafdd20a6
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-07-09 17:40:07 -0500

    Add information to the landing page.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,8 @@
-// import { useState } from 'react'
 import './App.css'
 import Header from '../Header/Header'
 import LandingPage from '../LandingPage/LandingPage'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -3,15 +3,15 @@ function LandingPage (){
     return(
         <>
             <div>
-                <div>
+                <div className="budget">
                     <h2>Why Budget?</h2>
-                    <p></p>
+                    <p> Budgeting is an essential building block to financial security and stability. It allows you to plan for big expenses and have peace of mind in the instance of an emergency. With changes in inflation and cost of living constantly rising, budgeting is still one of the most vital tools to obtaining financial freedom. learn more Below!</p>
                     <a href="https://www.investopedia.com/financial-edge/1109/6-reasons-why-you-need-a-budget.aspx">Purpose of Budgeting</a>
                 </div>
-                <div>
+                <div className="fruga">
                     <h2>Why Fruga?</h2>
-                    <p></p>
-                    <a href="https://www.consumerfinance.gov/about-us/blog/budgeting-how-to-create-a-budget-and-stick-with-it/">How to Budget</a>
+                    <p>Fruga was created with people in mind. Budgeting takes a lot of mental capacity, let us do it for you and offer suggestions in making your budget better</p>
+                    <a href="https://www.nerdwallet.com/article/finance/how-to-budget">How to Budget</a>
                 </div>
             </div>
             <div>
