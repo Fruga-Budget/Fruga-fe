d904697d60c3f56f9f3be298055b6451fc7e72cb
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-07-13 21:26:14 -0500

    set up testing expectations.

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -1,5 +1,20 @@
 describe('App', () => {
-  it('passes', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+  it('shows the landing page', () => {
+    
+  })
+  it('shows the header', () => {
     cy.visit('http://localhost:3000/')
+  })
+  it('shows landing information', () => {
+    
+  })
+  it('shows a generic pie chart', () => {
+    
+  })
+  it('has a button to get started', () => {
+    
   })
 })
--- cypress/e2e/Login.cy.ts
+++ cypress/e2e/Login.cy.ts
@@ -1,5 +1,49 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/log-in');
+  });
+  it('shows the login page', () => {
+    
+  })
+  it('Has an input for username', () => {
+   
+  })
+  it('Has an input for password', () => {
+    
+  })
+  it('it has buttons', () => {
+    
+  })
+  it('has a button to login', () => {
+    
+  })
+  it('has a button to register', () => {
+    
+  })
+  it('it will not let you log in without user/pass', () => {
+    
+  })
+})
+describe('register', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/log-in');
+  });
+  it('button for to register can be clicked', () => {
+    
+  })
+  it('it has an input for name', () => {
+   
+  })
+  it('Has an input for username', () => {
+    
+  })
+  it('has an input for password', () => {
+    
+  })
+  it('it will not allow you to register empty', () => {
+    
+  })
+  it('has a button to go back to login', () => {
+    
   })
 })
