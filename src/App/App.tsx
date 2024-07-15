b29708c05a7de83f91154e443b9ae144f374e4f1
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-07-15 13:18:40 -0500

    Begin API set up.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,33 @@
 import './App.css'
-import { BudgetInfo } from '../Interfaces'; 
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { APIData, BudgetInfo } from '../Interfaces'; 
 import Header from '../Header/Header'
 import LandingPage from '../LandingPage/LandingPage'
 import genericPieData from '../Pie/Mock';
 import Form from '../Form/Form';
 import Results from '../Results/Results';
 import LoginForm from '../Login/LogIn';
+import { getData } from '../API/APICalls'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function App() {
   const navigate = useNavigate();
+  const [data, setData] = useState<APIData>(null)
+  const [error, setError] = useState<Error | null>(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await getData();
+        setData(result);
+      } catch (err) {
+        setError(err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
 
   const handleFormSubmit = (budgetInfo: BudgetInfo) => { 
     localStorage.setItem('budgetInfo', JSON.stringify(budgetInfo));
