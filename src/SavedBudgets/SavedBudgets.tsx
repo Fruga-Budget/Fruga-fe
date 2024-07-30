d9629c99e74565ec39d4435a9161dcc0a8efab01
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-07-30 11:49:07 -0600

    enable viewing of budget details

--- src/SavedBudgets/SavedBudgets.tsx
+++ src/SavedBudgets/SavedBudgets.tsx
@@ -8,22 +8,52 @@ import mockSavedBudgets from './MockSavedBudgets'
 
 const SavedBudgets: React.FC = () => {  
     const [budgets, setBudgets] = useState<SavedBudget[]>([])
+    const [selectedBudgetId, setSelectedBudgetId] = useState<string | null>(null);
     // const [error, setError] = useState('')
     // const [isLoading, setIsLoading] = useState('')
 
     useEffect(() => {
         setBudgets(mockSavedBudgets)
         //api call will live here
-    }) 
+    }, []) 
+
+    const handleBudgetClick = (id: string) => {
+        setSelectedBudgetId(id === selectedBudgetId ? null : id); 
+      };
+
+      const closeBudgetDetail = () => {
+        setSelectedBudgetId(null);
+      };
     
     const allUserBudgets: JSX.Element[] = budgets.map((budget) => {
+        const isSelected = budget.id === selectedBudgetId;
+
             return (
-            <div className='single-budget'>
+            <div className='single-budget' key={budget.id} onClick={() => handleBudgetClick(budget.id)}>
+                <div className='budget-info'>
                 <p>Budget #{budget.id}</p>
                 <p>Income: ${budget.attributes.total_income}</p>
                 <p>Needs: ${budget.attributes.needs_total}</p>
                 <p>Wants: ${budget.attributes.wants_total}</p>
                 <p>Savings: ${budget.attributes.savings_total}</p>
+                <button 
+            className='view-button' 
+            onClick={() => handleBudgetClick(budget.id)}
+          >
+            {isSelected ? 'Close' : 'View'}
+          </button>
+                </div>
+                {isSelected && (
+          <div className='budget-details'>
+            
+            <div>
+            <h3>Advice:</h3>
+            {budget.attributes.advice.map((advice, index) => (
+              <p key={index}>{advice}</p>
+            ))}
+            </div>
+          </div>
+        )}
             </div>
             )
         }) 
