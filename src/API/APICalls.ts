d2d5156f63012e609b89c4cda5751b5272fcb9a9
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-07-30 13:39:35 -0500

    Fix errors in the console behind the API and routing.

--- src/API/APICalls.ts
+++ src/API/APICalls.ts
@@ -1,7 +1,8 @@
 import {APIData} from '../Interfaces'
 export async function getData(): Promise<APIData> {
+    const userId = localStorage.getItem('userId');
     try {
-        const response = await fetch('https://fruga-be-340d88ac3f29.herokuapp.com/');
+        const response = await fetch(`https://fruga-be-340d88ac3f29.herokuapp.com/api/v1/users/${userId}/advices`);
         
         if (!response.ok) {
             throw new Error(`error status: ${response.status}`);
