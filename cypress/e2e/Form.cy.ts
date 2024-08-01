990266aa9d85c3fb9cc497850317d6a8f34f0cc8
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-08-01 12:52:34 -0600

    add cypress testing to form component

--- cypress/e2e/Form.cy.ts
+++ cypress/e2e/Form.cy.ts
@@ -1,31 +1,59 @@
 /// <reference types="cypress" />
 
 describe('Form', () => {
-  beforeEach(() => {
+  before(() => {
+    cy.intercept('POST', 'https://fruga-be-340d88ac3f29.herokuapp.com/api/v1/users/*/advices', {
+      statusCode: 200,
+      body: {
+        data: {
+          id: "3",
+          type: "advice",
+          attributes: {
+            total_income: 5000,
+            needs_total: 1200,
+            wants_total: 1500,
+            savings_total: 500,
+            expenses: {
+              needs: [
+                { name: "Rent", amount: 1200, description: "", isNegotiable: false }
+              ],
+              wants: [
+                { name: "Vacation", amount: 1500, description: "" }
+              ],
+              savings: [
+                { name: "Emergency Fund", amount: 500, description: "" }
+              ]
+            },
+            advice: [
+              "Your submitted budget is being processed.",
+              "Here's a summary of your budget: Needs: Rent $1200; Wants: Vacation $1500; Savings: Emergency Fund $500."
+            ]
+          }
+        }
+      }
+    }).as('submitAdvice');
+
     cy.visit('http://localhost:3000/getting-started/1');
   });
-  it('has an input for gross income', () => {
-    
-  })
-  it('you can add income', () => {
-   
-  })
-  it('gross income is displayed', () => {
-    
-  })
-  it('has a clickable button to move one', () => {
-    
-  })
-  it('it shows three clickable buttons', () => {
-    
-  })
-  it('it allows you to add needs name and amount', () => {
-    
-  })
-  it('it allows you to add savings name and amount', () => {
-    
-  })
-  it('should have a submit button', () => {
-    
-  })
-})
+
+  it('should be able to add a budget', () => {
+    cy.get('input[name="grossIncome"]').should('be.visible').clear().type('5000').should('have.value', '5000');
+    cy.contains('Next').click();
+    cy.contains('Add Need').click();
+    cy.get('input[name="name"]').last().type('Rent').should('have.value', 'Rent');
+    cy.get('input[name="amount"]').last().clear().type('1200').should('have.value', '1200');
+    cy.contains('Next').click();
+    cy.contains('Add Want').click();
+    cy.get('input[name="name"]').last().type('Vacation').should('have.value', 'Vacation');
+    cy.get('input[name="amount"]').last().clear().type('1500').should('have.value', '1500');
+    cy.contains('Next').click();
+    cy.contains('Add Saving').click();
+    cy.get('input[name="name"]').last().type('Emergency Fund').should('have.value', 'Emergency Fund');
+    cy.get('input[name="amount"]').last().clear().type('500').should('have.value', '500');
+    cy.contains('Submit').click();
+    cy.wait('@submitAdvice').its('response.statusCode').should('eq', 200);
+  });
+});
+
+
+  
